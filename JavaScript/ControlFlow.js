daaa244763df18e1f9022b05a61a51e0b5e8993a
Fareez Murad <fareezphobia@gmail.com>
2023-03-26 21:49:44 +0800

    Noted on Control Flow is done.

--- JavaScript/ControlFlow.js
+++ JavaScript/ControlFlow.js
@@ -57,7 +57,7 @@ else console.log("Unknown User");
 // statement
 for (let i = 0; i <= 5; i++) {
   // i variable only run in this loop. Because it is part of the FOR LOOP.
-  //console.log("Hello World", i);  this loop will run 5 times.
+  //console.log('Hello World', i);  this loop will run 5 times.
   if (i % 2 !== 0) console.log("FOR LOOP" + i); // if the remainder is not 0, console.log will display the odd number. Because odd number divide by 2 will have a remainder.
 }
 
@@ -96,11 +96,11 @@ const person = {
 for (let key in person) console.log(key, person[key]);
 
 // Dot Notation
-//person.names;
+// person.names;
 // Cannot use this because we don't have propert called key in person object above
 
 // Bracket Notation
-//person["names"];
+// person['names'];
 // Use this because we don't know ahead of time what property we're going to access
 
 // Example of iterate over an array. But it is not ideal way
@@ -122,4 +122,205 @@ while (a <= 20) {
   }
   console.log(a);
   a++;
+}
+
+// Exercise 1
+let number = max(200, 11);
+
+function max(a, b) {
+  //   if (a > b) return a;
+  //   return b;
+  // Condition below is exactly the same. You can use either of them
+
+  return a > b ? a : b;
+}
+
+console.log(number);
+
+// Exercise 2
+let orientations = isLandscape(1920, 1080);
+console.log(orientations);
+
+function isLandscape(width, height) {
+  return width > height;
+}
+
+if (orientations === true) console.log("Landscape Mode");
+else console.log("Potrait Mode");
+
+// Exercise 3
+const output = fizzBuzz(90);
+console.log(output);
+
+function fizzBuzz(input) {
+  if (typeof input !== "number") return NaN;
+
+  if (input % 3 === 0 && input % 5 === 0) return "fizzBuzz";
+
+  if (input % 3 === 0) return "fizz";
+
+  if (input % 5 === 0) return "buzz";
+
+  return input;
+}
+
+// Exercise 4
+// Speed Limit = 70
+// 5km -> 1 point
+// Math.floor(1.3) to convert floating number into integers
+// 12 points -> suspended
+
+let condition = checkSpeed(125);
+console.log(condition);
+
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+
+  if (speed <= speedLimit + kmPerPoint) return "OK";
+
+  const points = Math.floor((speed - speedLimit) / kmPerPoint);
+
+  if (points >= 12) return "License suspended";
+  else return "Points = " + points;
+}
+
+// Exercise 5
+showNumbers(21);
+
+function showNumbers(limit) {
+  for (let i = 0; i <= limit; i++) {
+    // if (i % 2 === 0) console.log(i, 'EVEN');
+    // else console.log(i, 'ODD');
+    // Exactly same output as below method. Can use either one.
+
+    const message = i % 2 === 0 ? "EVEN" : "ODD";
+    console.log(i, message);
+  }
+}
+
+// Exercise 6
+// Count the number of TRUTHY value in an array
+
+const array = ['f', 1, 2, 0, 200]; 
+console.log(countTruthy(array));
+
+function countTruthy(array) {
+  let count = 0; // count the array start from zero
+  for (let value of array) // to iterate over the items in array
+    if (value) // check if value is TRUTHY or FALSEY
+    count++; // if TRUTHY, count will be increase by 1. if FALSEY, value will stay and iterate over the array.
+  return count;
+}
+
+// Exercise 7
+const movie = {
+  title: 'a',
+  releaseYear: 2018,
+  rating: 4.5,
+  director: 'b'
+};
+
+showProperties(movie);
+
+function showProperties(obj){
+  for (let properties in obj)
+   if (typeof obj[properties] === 'string') // compare the type of properties of an object. If the properties is STRING, it will show on the console. If not, it will iterate over the function.
+    console.log(properties, obj[properties]);
+}
+
+// Exercise 8
+console.log(sum(10));
+
+// Multiple of 3: 3, 6, 9 <10
+// Multiples of 5: 5,10 <10
+// The sum of all of above numbers
+
+function sum(limit){
+  let sum = 0; // Initial value of sum
+
+  for (let i = 0; i <= limit; i++) // Iterate over the value of variable i
+    if (i % 3 === 0 || i % 5 === 0) // if i divided by 3 or 5 and not have a remainder, SUM + i. Then iterate over.
+    // But if i have a remainder, if will IGNORE SUM + 1 iterate over the for loop.
+      sum += i; // sum is a variable to store the value of i that dont have a remainder.
+
+  return sum; // this will return the value back to sum.
+}
+
+// Exercise 9
+// Calculate average of an array.
+const marks = [80, 80, 50];
+
+// Average = 70
+
+// 0-59: F
+// 60-69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
+
+console.log(calculateGrade(marks));
+
+function calculateGrade(marks){
+  const average = calculateAverage(marks); // call calculateAverage function.
+
+  if (average < 60) return 'F'
+  if (average < 70) return 'D'
+  if (average < 80) return 'C'
+  if (average < 90) return 'B'
+  return 'A'
+}
+
+function calculateAverage(array){
+  let sum = 0; // Initial value of sum
+  for (let value of array) // use FOR OF loop for iterate over an array.
+    sum += value; // value of array will be added to sum and will iterate over if the value is exist.
+  return sum / array.length; // return the value to sum variable.
+}
+
+// Exercise 10
+showStars(3); // Call out the function.
+
+function showStars(rows) {
+  for (let row = 1; row <= rows; row++) {
+  // Use FOR loop for iteration.  
+    let starPattern = ''; // set variable to empty string.
+    for (let i = 0; i < row; i++) // set DUMMY iteration reference to row FOR loop to add * string count.
+      starPattern += '*'; // add * string to each row.
+    console.log(starPattern);
+  }
+}
+
+// Exercise 11
+let value = 20;
+
+showPrimeNumbers(value);
+
+// Prime Numbers (whose factors are ony 1 and itself)
+
+function showPrimeNumbers(limit) {
+  for (let number = 2; number <= limit; number++) // iterate this FOR loop starting from 2 -> limit. If isPrime = true, console will show the all the prime number. if isPrime = false, the number will now show on console.
+    if (isPrime(number)) console.log(number); // calling isPrime function to this function.
+}
+
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++) 
+  // iterate over starting from 2 -> number. Every factor number starting from 2, will check with every 'number' number if they have a remainder.
+  // if the iteration find the number with the remainder of 0, it will return FALSE. Hence, it will not count as prime number.
+  // if the iteration did not find any number with remainder untill the 'limit' value reached, it will return TRUE.
+    if (number % factor === 0) 
+      return false; // false if iteration find a number with 0 remainder.
+  
+  return true; // true if iteration did not find any number with 0 remainder.
+}
+
+// FOR UNDERSTANDING FOR LOOP PURPOSE in this primeNumber case
+a(value);
+function a (b){
+ for (let c = 2; c <= b; c++) {
+  for (let d = 2; d < c; d++){
+  console.log('value of number = '+ c);
+  console.log('value of factor = '+ d);
+  }
+ }
 }
--- JavaScript/Exercise.js
+++
@@ -1,9 +0,0 @@
-let a = "red";
-let b = "blue";
-
-let x = a;
-a = b;
-b = x;
-
-console.log(a);
-console.log(b);
