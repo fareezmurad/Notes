589b95dc0b3371d4c01a8ebf20b5adc33f29f442
Fareez Murad <fareezphobia@gmail.com>
2023-03-30 14:17:31 +0800

    Notes of value vs reference type.

--- JavaScript/Objects.js
+++ JavaScript/Objects.js
@@ -98,4 +98,88 @@ console.log(rectangle); // Show object after delete properties
 // Reference the function that was used to construct or create the object.
 // Every object has a constructor property and that references a function that was use to create that object.
 
+// FUNCTION ARE OBJECTS
 
+// Example of constructor function
+function Sphere(radius, perimeter) {
+  this.radius = radius;
+  this.perimeter = perimeter;
+  this.draw = function() {
+    console.log('draw');
+  }
+}
+
+// These 2 are basicly the same
+Sphere.call({}, 2, 5);
+const another = new Sphere(2, 5);
+
+// Use this to pass an array
+Sphere.apply({}, [1, 2, 3])
+
+// VALUE VS REFERENCE TYPE
+/* 
+Value types (Primitive)
+1. Number
+2. String
+3. Boolean
+4. Symbol
+5. Undefined
+6. null
+*/
+
+let a = 10; // Initialize a = 10
+let b = a; // Initialize b = a. b = 20
+
+a = 20; // Reassign a = 20
+/* 
+Current value:
+a = 20
+b = 10
+*/
+
+// a and b are 2 independent variable. The value of each variable are stored independently in the variable. If we reassigned the value of a, b is not affected. It will still store the initial value that was assign to b which is in this case was 10.
+
+// Example
+let num = 10; // Initialize num variable
+function increase(num) {
+  num++;
+}
+
+increase(num); // call the function
+console.log(num); // show the value of num in the console after being called
+// Initial value = 10
+// Final value = 10
+
+/*
+Reference Type
+1. Object
+2. Function
+3. Array
+*/
+
+let c = { value: 10}; // Initialize value = 10
+let d = c; // Initialize property of d = c. d.value = 10
+
+c.value = 20; // Reassign value = 20.
+/*
+Current value:
+c = { value: 20}
+d = { value: 20}
+*/
+
+// The properties of an object are contain within the memory. If we reassign the property of c, the property of d also will be updated to other object
+
+let obj = {value: 10} // Initialize obj properties
+
+function increase(obj) {
+  obj.value++;
+}
+
+increase(obj); // call the function
+increase(obj); // call the function
+console.log(obj); // show the properties value after being called twice
+// Initial value = 10
+// Final value = 12
+
+// Primitives are copied by their value
+// Objects are copied by their reference
