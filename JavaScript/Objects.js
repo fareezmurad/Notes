a374dd266c50418868ff43461e62830e7c6f679e
Fareez Murad <fareezphobia@gmail.com>
2023-03-31 16:26:27 +0800

    Add exercise in object.js

--- JavaScript/Objects.js
+++ JavaScript/Objects.js
@@ -9,7 +9,7 @@ let x = 1;
 let y = 2;
 
 // All these variable are highly related. They represent a cicle.
-// Better approach is put these variables inside of an object. 
+// Better approach is put these variables inside of an object.
 
 // This is called Object Oriented Programming (OOP)
 // Collection of an object that talk to each other to perform some functionality.
@@ -20,9 +20,10 @@ const circle = {
     y: 1,
   },
   isVisible: true,
-  draw: function(){ // Function also can be store inside of an object.
+  draw: function () {
+    // Function also can be store inside of an object.
     console.log('draw'); // Function inside of an object is called METHOD.
-  }
+  },
 };
 
 // Access them using dot notation
@@ -36,9 +37,10 @@ circle.draw();
 function createCircle(radius) {
   return {
     radius, // If key and value are the same, we can remove the value. In this case: radius, = radius: radius,
-    draw() { // Other style to type function inside an object
+    draw() {
+      // Other style to type function inside an object
       console.log('draw');
-    }
+    },
     /*draw: function(){
       console.log('draw');
     }*/
@@ -59,11 +61,11 @@ console.log(circle2);
 For example: OneTwoThreeFour, CreateCircle. */
 
 function Circle(radius) {
-  this.radius = radius; // THIS: refer to object. In this case, it's 'Circle' object. 
+  this.radius = radius; // THIS: refer to object. In this case, it's 'Circle' object.
   // In other words, it is equal to Circle.radius or Circle['radius'].
-  this.draw = function() {
+  this.draw = function () {
     console.log('draw');
-  }
+  };
 }
 
 const circle3 = new Circle(1); // NEW: is to create new/empty object based on constructor function
@@ -74,8 +76,9 @@ console.log(circle3);
 // DYNAMIC NATURE OF OBJECTS
 // Can add or remove properties of an object
 
-const rectangle = { // Initial properties of an object
-  corner: 4
+const rectangle = {
+  // Initial properties of an object
+  corner: 4,
 };
 // You can dynamically change the object properties but you cannot reassign the constant variable of an object. In this case, constant variable is rectangle.
 /* 
@@ -84,7 +87,7 @@ rectangle = {};
 */
 
 rectangle.color = 'red'; // Add properties to an object
-rectangle.draw = function() {} // Add method to an object (using dot notation)
+rectangle.draw = function () {}; // Add method to an object (using dot notation)
 
 console.log(rectangle); // Show object after added properties
 
@@ -104,9 +107,9 @@ console.log(rectangle); // Show object after delete properties
 function Sphere(radius, perimeter) {
   this.radius = radius;
   this.perimeter = perimeter;
-  this.draw = function() {
+  this.draw = function () {
     console.log('draw');
-  }
+  };
 }
 
 // These 2 are basicly the same
@@ -114,7 +117,7 @@ Sphere.call({}, 2, 5);
 const another = new Sphere(2, 5);
 
 // Use this to pass an array
-Sphere.apply({}, [1, 2, 3])
+Sphere.apply({}, [1, 2, 3]);
 
 // VALUE VS REFERENCE TYPE
 /* 
@@ -157,7 +160,7 @@ Reference Type
 3. Array
 */
 
-let c = { value: 10}; // Initialize value = 10
+let c = { value: 10 }; // Initialize value = 10
 let d = c; // Initialize property of d = c. d.value = 10
 
 c.value = 20; // Reassign value = 20.
@@ -169,7 +172,7 @@ d = { value: 20}
 
 // The properties of an object are contain within the memory. If we reassign the property of c, the property of d also will be updated to other object
 
-let obj = {value: 10} // Initialize obj properties
+let obj = { value: 10 }; // Initialize obj properties
 
 function increase(obj) {
   obj.value++;
@@ -184,31 +187,27 @@ console.log(obj); // show the properties value after being called twice
 // Primitives are copied by their value
 // Objects are copied by their reference
 
-
 // ENUMERATING PROPERTIES OF AN OBJECT
 
 const square = {
   corner: 4,
-  draw () {
+  draw() {
     console.log('draw square');
-  }
+  },
 };
 
-for (let key in square)
-  console.log(key, square[key]);
+for (let key in square) console.log(key, square[key]);
 /* for (let key of square)
-  console.log(key); */ 
-  // Will got error. Because it is not iterable.
-  // For Of loops only can be use on array or maps. Not on object
-  // Object is not iterable
+  console.log(key); */
+// Will got error. Because it is not iterable.
+// For Of loops only can be use on array or maps. Not on object
+// Object is not iterable
 
 // But we can use For Of loop this way:
-for (let key of Object.keys(square))
-  console.log(key);
+for (let key of Object.keys(square)) console.log(key);
 // Object.keys : this method returns a string of array which contains all the properties and method of an object.
 
-for (let entry of Object.entries(square))
-  console.log(entry);
+for (let entry of Object.entries(square)) console.log(entry);
 // Object.entries : this method returns each entry value pair as an array.
 
 if ('radius' in square) console.log('yes');
@@ -219,10 +218,10 @@ else console.log('No');
 
 const pentagon = {
   corner: 5,
-  draw: function() {
+  draw: function () {
     console.log('draw pentagon');
-  }
-}
+  },
+};
 /* This is OLD METHOD
 const another1 = {};
 console.log(another1); // This object currently have empty property
@@ -242,7 +241,7 @@ const another1 = Object.assign({
 }, pentagon) */
 
 // Much Simpler Method
-const another1 = { ...pentagon}; // Called as spread operator (3 dots)
+const another1 = { ...pentagon }; // Called as spread operator (3 dots)
 // It take all properties and method of object and put them in between the curly braces.
 
 console.log(another1); // This object after copy the properties of pentagon
@@ -302,7 +301,7 @@ msgLengthChk (30);
 */
 
 // String Object
-const anotherString = new String('hi') // typeof anotherString = 'object'
+const anotherString = new String('hi'); // typeof anotherString = 'object'
 
 // TEMPLATE LITERALS
 /*
@@ -313,7 +312,7 @@ Template ``
 */
 
 const message1 = 'This is\n my first message';
-// Above string is basicly same with below string in terms of the output on the console. 
+// Above string is basicly same with below string in terms of the output on the console.
 // But the below string is much cleaner.
 // Below is what we called TEMPLATE LITERALS
 const anothermessage = `This is 
@@ -323,8 +322,7 @@ const name = 'Timmy';
 /* const message2 = 'Hi' + name + ',\n'; 
 Instead using this, better use template literal for cleaner and understandable code. */
 
-const message2 = 
-`Hi ${name},
+const message2 = `Hi ${name},
 
 Thank you for joining my mailing list.
 
@@ -338,10 +336,143 @@ Fareez`;
 // Objects represent a single moment in time in a platform-independent format.
 
 // This is how we create DATE object
-const now = new Date()
+const now = new Date();
 const date1 = new Date('March 30 2023 23:03');
 const date2 = new Date(2023, 2, 30, 23, 3);
 
 // Date have bunch of GET and SET method
 now.setFullYear(2020);
 // Example to set YEAR
+
+// Exercise 1
+/* Address Object
+1. street
+2. city
+3. zipcode */
+// Create a Funtion showAddress(address)
+
+const address = {
+  street: 'Hala Klebang Restu 36',
+  city: 'Chemor',
+  zipcode: 31200,
+};
+
+function showAddress(address) {
+  for (let key in address) console.log(key, address[key]);
+}
+
+showAddress(address);
+
+// Exercise 2
+// Use Factory and Constructor Funtions
+
+/* Address Object
+1. street
+2. city
+3. zipcode */
+
+// Factory Functions
+function createAddress(street, city, zipcode) {
+  return {
+    street,
+    city,
+    zipcode,
+  };
+}
+
+const address1 = createAddress('Hala 36', 'Chemor', 31200);
+console.log(address1);
+
+// Constructor Functions
+function Address(street, city, zipcode) {
+  (this.street = street), (this.city = city), (this.zipcode = zipcode);
+}
+
+const address2 = new Address('Hala 36', 'Chemor', 31200);
+console.log(address2);
+
+// Exercise 3
+// Object Equality
+
+function Address(street, city, zipcode) {
+  (this.street = street), (this.city = city), (this.zipcode = zipcode);
+}
+
+const address1 = new Address('Hala 36', 'Chemor', 31200);
+const address2 = new Address('Hala 36', 'Chemor', 31200);
+
+function areEqual(address1, address2) {
+  return (
+    address1.street === address2.street &&
+    address1.city === address2.city &&
+    address1.zipcode === address2.zipcode
+  );
+}
+
+function areSame(address1, address2) {
+  return address1 === address2;
+}
+
+console.log(areEqual(address1, address2)); // return true. because the properties of this objects are the same
+console.log(areSame(address1, address2)); // return false. Because it is 2 different object
+
+// Exercise 4
+// Blog Post Object
+/* 
+1. title
+2. body
+3. author
+4. views
+5. comment (author, body)
+6. isLive (true or false)
+*/
+
+const blogPost = {
+  title: 'My Blog Post',
+  body: 'nothing',
+  author: 'You',
+  views: 1,
+  comments: [{ author: 'me', body: 'content' }], // object in an object
+  isLive: true,
+};
+
+// Exercise 5
+// Blog Post Object using constructor functions
+/* 
+1. title
+2. body
+3. author
+4. views
+5. comment (author, body)
+6. isLive (true or false)
+*/
+
+let post = new CreatePost(
+  'This is title',
+  'This is body',
+  'This is your author'
+);
+
+console.log(post);
+
+function CreatePost(title, body, author) {
+  this.title = title;
+  this.body = body;
+  this.author = author;
+  this.views = 0; // Should initialize to 0 because views is not something you want configure. It count automatically as audience views your blog post
+  this.comments = []; // same with comment. set to empty string
+  this.isLive = false;
+}
+
+// Exercise 6
+// Price Range Object
+
+// inexpensive, moderate, pricey
+
+const priceRange = [
+  { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
+  { label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 },
+  { label: '$$$', tooltip: 'Pricey', minPerPerson: 21, maxPerPerson: 50 },
+];
+
+let restaurants = [{ averagePerPerson: 5 }];
