431ab124b680a25c8556352977eeb95a36941cfa
Fareez Murad <fareezphobia@gmail.com>
2023-04-07 20:36:38 +0800

    Notes on function declaration vs expression.

---
+++ JavaScript/Functions.js
@@ -0,0 +1,19 @@
+// FUNCTION DECLARATIONS VS EXPRESSIONS
+// Function Declaration
+function walk() {
+  console.log('walk');
+}
+
+// Anonymous Function Expression
+let run = function () {
+  console.log('run');
+};
+
+run();
+
+// Name Function Expression
+let joggging = function jog() {
+  console.log('jog');
+};
+
+joggging();
