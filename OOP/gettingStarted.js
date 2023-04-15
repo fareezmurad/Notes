7817bdd9a64d4c98e96796fe77487a5fe9a6f7c4
Fareez Murad <fareezphobia@gmail.com>
2023-04-15 20:23:43 +0800

    Notes on 4 pillar of OOP.

--- OOP/gettingStarted.js
+++ OOP/gettingStarted.js
@@ -1,3 +1,21 @@
 // Object Oriented Programming is a style of programiing that is centered around objects rather than functions.
 
-//
+// FOUR PILLAR OF OOP
+// 1. encapsulation
+// 2. abstraction
+// 3. inheritance
+// 4. polymorphism
+
+// 1. Encapsulation
+// Combine a group of related variables and functions into an object.
+
+// 2. Abstraction
+// Hide method and properties from the outside for:
+// interface of those objects is simpler
+// reduce the impact of change
+
+// 3. Inheritance
+// Mechanism that allows you to eliminate redundant code.
+
+// 4. Polymorphism
+// Refactor ugly switch/case statements
