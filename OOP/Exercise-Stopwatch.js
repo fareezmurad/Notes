90d823935b82c9788202e6d08131f312eb6740a5
Fareez Murad <fareezphobia@gmail.com>
2023-04-18 20:29:47 +0800

    Complete exercise (Build a stopwatch constructor).

---
+++ OOP/Exercise-Stopwatch.js
@@ -0,0 +1,47 @@
+function Stopwatch() {
+  // Variable to calculate things in function scope
+  let startTime = 0;
+  let endTime = 0;
+  let isRunning = false;
+
+  const duration = () => {
+    if (isRunning) {
+      const getTime = new Date().getTime();
+      return (getTime - startTime) / 1000;
+    } else return (endTime - startTime) / 1000;
+  };
+
+  // To start the stopwatch using getTime() method for input to get unique current time in milliseconds
+  this.start = () => {
+    if (isRunning) throw new Error('Stopwatch is already started');
+    else {
+      isRunning = true;
+      const getTime = new Date().getTime();
+      startTime += getTime;
+    }
+  };
+
+  // To stop the stopwatch using getTime() method for input to get unique current time in milliseconds
+  this.stop = () => {
+    if (!isRunning) throw new Error('Stopwatch is already stopped');
+    else {
+      isRunning = false;
+      const getTime = new Date().getTime();
+      endTime += getTime;
+    }
+  };
+
+  // Initialize variable back to Initial value for reset button
+  this.reset = () => {
+    if (isRunning) this.stop();
+    startTime = 0;
+    endTime = 0;
+  };
+
+  // Make it variable accessible from outside scope(read-only). Cannot be overwritten.
+  Object.defineProperty(this, 'duration', {
+    get: () => {
+      return duration();
+    },
+  });
+}
